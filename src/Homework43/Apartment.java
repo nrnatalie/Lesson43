351a46fff0968d82026cc3f7a40dc83e7b0cf0c3
Nathalie Russo <johndoe@example.com>
2023-03-21 11:32:26 +0200

    Correct Apartment constructor

--- src/Homework43/Apartment.java
+++ src/Homework43/Apartment.java
@@ -1,8 +1,11 @@
 package Homework43;
 
 public class Apartment extends House {
-
-  public Apartment(String direction) {
+  // При создании многоквартирного дома дополнительно принимайте в конструкторе целое число -
+  // количество подъездов.
+  private int entrances;
+  public Apartment(String direction, int entrances) {
     super(direction);
+    this.entrances = entrances;
   }
 }
